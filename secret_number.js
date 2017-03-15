954125fd146506391ea97dbfad0691d9a0d8c4c4
ctsygiel <ctsygiel@gmail.com>
2017-03-14 18:49:23 -1000

    Made all tests pass

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,12 @@
 'use strict';
 module.exports = function() {
-  
+
+  //PRIVATE
+  var secretNumberGenerator = Math.floor((Math.random() * 1000000) + 1);
+
+  //PUBLIC
+ function secretNumber(){
+  return secretNumberGenerator;
+ }
+return secretNumber;
 };
